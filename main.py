43599f1d7aa0222133ee81322177616c035a7518
Karol Pisarski <karol.pisarski@outlook.com>
2024-05-25 12:48:48 +0200

    Added h264 and h265 encoders

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import os
 from my_image import MyImage
-from encoder import Encoder
+from encoder import H264Encoder, H265Encoder
 
 input_image_path = "pictures/1.png"
-output_video_path = "output/output_video.avi"
+output_video_path_h264 = "output/output_video_h264.mp4"
+output_video_path_h265 = "output/output_video_h265.mp4"
 grid_size = 256
 frame_size = 256
 
 def main():
-    output_dir = os.path.dirname(output_video_path)
+    output_dir = os.path.dirname(output_video_path_h264)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     
     image = MyImage(input_image_path)
-    image.show()
+    # image.show()
     
     tiles = image.split_image(grid_size)
     
-    encoder = Encoder(frame_size)
-    encoder.encode_as_video(tiles, output_video_path)
+    # Calcualte original data size
+    original_size = sum(tile.nbytes for tile in tiles)
+    print(f"Original data size: {original_size} bytes")
     
-    print(f"Video saved to {output_video_path}")
+    # H.264 encoding
+    encoder_h264 = H264Encoder(frame_size)
+    encoder_h264.encode_as_video(tiles, output_video_path_h264)
+    if os.path.exists(output_video_path_h264):
+        compression_ratio_h264 = encoder_h264.calculate_compression_ratio(original_size, output_video_path_h264)
+        print(f"Video saved to {output_video_path_h264}")
+        print(f"Compression ratio (H.264): {compression_ratio_h264:.2f}")
+
+    # H.265 encoding
+    encoder_h265 = H265Encoder(frame_size)
+    encoder_h265.encode_as_video(tiles, output_video_path_h265)
+    if os.path.exists(output_video_path_h265):
+        compression_ratio_h265 = encoder_h265.calculate_compression_ratio(original_size, output_video_path_h265)
+        print(f"Video saved to {output_video_path_h265}")
+        print(f"Compression ratio (H.265): {compression_ratio_h265:.2f}")
 
 if __name__ == "__main__":
     main()
